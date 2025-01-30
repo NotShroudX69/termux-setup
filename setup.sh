25d1a2cf8000a7eb46d8f0c969a68411f69d3c5c
𝘕𝘰𝘵𝘚𝘩𝘳𝘰𝘶𝘥 <NotShroudX69@Proton.Me>
2025-01-30 15:17:41 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -163,6 +163,15 @@ setup_kali() {
         termux-x11-nightly \
         pulseaudio
 
+    # Install Kali Linux in Proot-Distro
+    mkdir -p \$PREFIX/etc/proot-distro/
+    cat <<EOC > \$PREFIX/etc/proot-distro/kali.sh
+    # Kali-Linux Arm64 Edition
+    DISTRO_NAME="kali"
+    DISTRO_COMMENT="Kali-Linux Arm64 (Official Edition)"
+    TARBALL_URL['aarch64']="https://kali.download/nethunter-images/current/rootfs/kali-nethunter-rootfs-minimal-arm64.tar.xz"
+    TARBALL_SHA256['aarch64']="fad2607aa841015eac428c0f23289b1a3f8039c5377d16f4fc46206e22d6c45a"
+
     proot-distro install kali
 
     clear
